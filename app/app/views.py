24d3126160aba8350d9ff7dc12417a539fc2f068
Bartosz Guzik <bguz1kk@gmail.com>
2024-06-02 15:19:06 +0200

    Delete system

--- app/app/views.py
+++ app/app/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render
 from app.models import System
 from rest_framework.views import APIView
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from app.serializers import SystemsSerializer
+from django.shortcuts import get_object_or_404
 
 
 class SystemsView(APIView):
     """ All systems view """
     
-    def get(self, request):
+    def get(self, request) -> JsonResponse:
         """ All system get """
         #user = self.request.user
         systems = System.objects.all()
         response = SystemsSerializer(systems, many=True)
         return JsonResponse(response.data, safe=False)
+    
+
+class SingleSystemView(APIView):
+    """ Single system view """
+    
+    def delete(self, request, systemID:int) -> HttpResponse:
+        """_summary_
+
+        Args:
+            systemID (int): system id
+
+        Returns:
+            HttpResponse: HTTP 204 - succesfull delete
+                          HTTP 404 - object not found
+        """
+        system = get_object_or_404(System, pk=systemID)
+        system.delete()
+        return HttpResponse(status=204)
