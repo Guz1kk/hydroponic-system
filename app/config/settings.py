732ff5ffe861d0a2c34d69f4416890bfa122276c
Bartosz Guzik <bguz1kk@gmail.com>
2024-06-02 13:59:52 +0200

    Postgres db

--- app/config/settings.py
+++ app/config/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "luna_db",
+        "HOST": "localhost",
+        "USER": "admin",
+        "PORT": "5432",
+        "PASSWORD": "admin",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
