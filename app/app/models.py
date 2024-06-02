b5e033e81acab3ae6bf9d3a92d4607adefd77855
Bartosz Guzik <bguz1kk@gmail.com>
2024-06-02 14:47:49 +0200

    System model

--- app/app/models.py
+++ app/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class System(models.Model):
+    """ System model """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    
+    class Meta:
+        unique_together = ['user', 'name']
+        
+    def __str__(self) -> str:
+        return self.name
