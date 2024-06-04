22b2d8b0f7110d58580f48c9828f5c76d64aa892
Bartosz Guzik <bguz1kk@gmail.com>
2024-06-05 00:05:23 +0200

    Delete logout

--- app/auth/urls.py
+++ app/auth/urls.py
@@ -6,5 +6,4 @@ from . import views
 urlpatterns = [
     path('', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh', TokenRefreshView.as_view(), name='token_refresh'),    
-    path('logout', views.LogoutView.as_view(), name='logout user')
 ]
